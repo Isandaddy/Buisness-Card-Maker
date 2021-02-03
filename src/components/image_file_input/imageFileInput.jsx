60eee9cc7d8a831afe55632823258333a9355fe3
Seung Tack Jo <jstack87215@gmail.com>
2021-02-03 22:24:57 +0900

    edit card-editor-form and css

---
+++ src/components/image_file_input/imageFileInput.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './imageFileInput.module.css';
+
+const ImageFileInput = (props) => (
+        <button>Image</button>
+    );
+
+export default ImageFileInput;
